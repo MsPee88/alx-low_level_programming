f153ade755f58a8ab0b2701e3e9a96496d91e88f
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-17 02:07:03 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Prints out a string to stdout.
+ * Return 0 if sucess.
+ */
+int main (void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+
+}
