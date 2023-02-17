6661f53c4319c247a3f1c3016e4ee57714814421
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-17 00:50:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main (void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
