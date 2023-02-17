bdfb1c13e8bbce2657a7857b69d642cb148dc704
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-17 05:52:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",sizeof("and that piece of art is useful\"-Dora Korpar,2015-10-19"));
+	(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",
+	 sizeof("and that piece of art is useful\"-Dora Korpar,2015-10-19"));
 	return(1);
 }
