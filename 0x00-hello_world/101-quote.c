c9674a5b2b035f82a0bde44fb9f745b1f30df34a
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-17 05:49:13 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",sizeof("and that piece of art is useful\"-Dora Korpar,2015-10-19"));
+	return(1);
+}
