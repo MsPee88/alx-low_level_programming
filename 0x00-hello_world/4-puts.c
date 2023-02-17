b4d0fefe1ee3a692ee0c75c9d842b7ce65001188
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-17 00:58:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Entry Point
+ * main - Prints out a sting to stdout.
  *
  * Return: Always 0 (Sucess)
  */
 int main (void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
