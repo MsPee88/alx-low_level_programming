8193d18bd10d5ee321ae03d0c2d882e30450fe19
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-23 23:30:05 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
 /**
- * print_line - print n underscore characters
- * @n: number of times to print _
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
+ * Return: empty
  */
+
 void print_line(int n)
 {
-	int i;
+	int x;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('\n');
 	}
-	_putchar('n\');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+	}
 }
