3d9e952b65a4fd22cb6570e8dff291447680b3c0
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-23 22:24:10 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,9 @@
  * more_numbers - Print 10 times the number since 0 up to 14
  * Return: 10 times of the number since 0 up to 14
  */
+
 void more_numbers(void)
+
 {
 	int x, y;
 
@@ -15,7 +17,9 @@ void more_numbers(void)
 			if (y > 9)
 					{
     _putchar((y / 10) + '0);
+					}
     _putchar((y % 10) + '0);
+		}
     _putchar('\n');
 					}
 }
