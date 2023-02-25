9eb81f6cb8ee5474b997ca95bbc9e034aa93de82
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-25 13:18:53 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,13 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
+
 	if (last < 0);
-	{
-		last = last * -1;
-	}
+
+	else	
+		
+	last = last * -1;	
+
 	_putchar(last + '0');
 	return (last);
 }
