738058b113b938b6dd64336c157010cd24b3f6a3
zvanyadza <zamandapriviledge@yahoo.com>
2023-02-27 23:47:50 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,9 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to 98
+ *
  * @n: input
+ *
  * Return: n
  */
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
-#include<unistad.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,5 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
