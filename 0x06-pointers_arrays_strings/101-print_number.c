d2467f91a1ba21c62c8d16be8bd5e2432ce5516e
zvanyadza <zamandapriviledge@yahoo.com>
2023-03-01 23:26:11 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,6 +7,7 @@
  */
 
 void print_number(int n)
+
 {
 	unsigned int n1;
 
@@ -14,12 +15,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+	_putchar('-');
+	n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10);
+	print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
