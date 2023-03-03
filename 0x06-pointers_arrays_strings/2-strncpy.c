d172f487d5c3500adbe476a71d96d869957438f7
zvanyadza <zamandapriviledge@yahoo.com>
2023-03-02 23:52:15 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
-
 /**
- * _strncpy - Copies at most an inputed number
- * of bytes from string src into dest.
- * @dest: The buffer storing the strong copy.
- * @src: The maximum numberof bytes to be copied from src.
+ * _strncpy - Copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  * Return: Apointer to the resulting string dest
- *
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
-	int index = 0, src_len = 0;
+	int j;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
+	j = 0
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
