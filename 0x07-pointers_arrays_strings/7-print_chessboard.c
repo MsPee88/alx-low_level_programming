ca2556b8374c3bde2381c15fd55fd9bec06275b0
zvanyadza <zamandapriviledge@yahoo.com>
2023-03-06 10:43:18 -0800

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,4 +5,14 @@
  * @a: array
  * Return: Always 0 (Success)
  */
-void print_chessboard(char(*a)[0])
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
