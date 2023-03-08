b36df9152c841ee67bf62583b566dbd450fc73ab
zvanyadza <zamandapriviledge@yahoo.com>
2023-03-08 05:19:13 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Print a string followed by a new line 
+ * _puts_recursion - Prints a string followed by a new line 
  * @s: the string to print
  *
  * Return: Nothing
