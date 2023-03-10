b56317a1265ec6490ab41c5da0192ceb8d7eafdb
zvanyadza <zamandapriviledge@yahoo.com>
2023-03-10 04:55:10 -0800

    libmy.a

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,10 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+char *_strncat(char *dest, char *src, unsigned int n)
+{
+	return strncat(dest, src, n);
+}
